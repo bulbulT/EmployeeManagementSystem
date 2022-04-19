9b346c3ac03f57c89a17fbe62596dc70ae787fa0
bulbulT <bulbultripathy07@gmail.com>
2022-04-19 17:43:21 +0530

    Initial Commit

---
+++ src/main/java/com/emloyeesystemapi/EmloyeeSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.emloyeesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmloyeeSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmloyeeSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emloyeesystemapi/EmloyeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emloyeesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmloyeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
